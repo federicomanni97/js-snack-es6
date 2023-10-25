33be846b2ec94c1888963a355f53657689a8f476
federicomanni97 <manni_federico@libero.it>
2023-10-25 17:20:21 +0200

    snack 4 on console

--- js/script.js
+++ js/script.js
@@ -160,3 +160,58 @@ const biciLeggera = Math.min(...peso);
 console.log(biciLeggera);
 const risposta = `La bici più leggera pesa ${biciLeggera}kg'`;
 console.log(risposta);
+
+
+//! SNACK 4
+//! Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+//! Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+//! Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+//! Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
+const soccerTeams = [
+        {
+            name: 'Besiktas',
+            puntiFatti: '0',
+            falliSubiti: '0'
+
+        },
+        {
+            name: 'Galatasaray',
+            puntiFatti: '0',
+            falliSubiti: '0'
+        },
+        {
+            name: 'Istanbulspor',
+            puntiFatti: '0',
+            falliSubiti: '0'
+        },
+        {
+            name: 'Fenerbahce',
+            puntiFatti: '0',
+            falliSubiti: '0'
+        },
+        {
+            name: 'Trabzonspor',
+            puntiFatti: '0',
+            falliSubiti: '0'
+        }
+
+]
+
+const finalResult = [];
+
+soccerTeams.forEach((element, index) =>{
+    element.puntiFatti = getRndInteger(2, 69);
+    element.falliSubiti = getRndInteger(2, 69);
+    const {name} = soccerTeams[index];
+    const {falliSubiti} = soccerTeams[index];
+    finalResult.push({name, falliSubiti})
+})
+console.log(soccerTeams);
+console.log(finalResult);
+
+//UTILITY
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+};
